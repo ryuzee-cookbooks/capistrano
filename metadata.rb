e3310cc7c8005095bac84f85622d4d060dd83b71
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-11-24 06:26:01 +0900

    clarify license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "capistrano"
 maintainer       "ryuzee"
 maintainer_email "ryuzee@gmail.com"
-license          "All rights reserved"
+license          "MIT"
 description      "Installs/Configures capistrano"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
